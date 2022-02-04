9306dab6002c882c724f0decfcdd1178b13779c5
mathg15 <69726961+mathg15@users.noreply.github.com>
2022-02-04 16:35:36 +0100

    Update test.py
    + Comments

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 import moosh as ms
 
+# Définition des couches
 Layer1 = ms.Layer(1, 1, 100).layer()
 Layer2 = ms.Layer(2, 1, 100).layer()
 
+# Définition de la structure de la multicouche
 structure = ms.Structure(
     Layer1,
     Layer2
 ).structure()
 
+# On utilise ensuite la fonction Angular pour construire le spectre
 ms.Angular(structure, 0, 600).angular()
